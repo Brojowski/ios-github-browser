fb059c057fbd770a501b7c5756460d114bea0974
Alex Gajowski <alex@Alexs-MacBook-Pro.local>
2019-04-04 18:50:45 -0400

    list gist files

--- Podfile
+++ Podfile
@@ -3,4 +3,5 @@ use_frameworks!
 
 target 'ios-github-browser' do
     pod 'Highlightr', '~> 2.1.0'
+    pod "NBNRequestKit"
 end 
--- ios-github-browser/ViewController.swift
+++ ios-github-browser/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        Octokit().gists(owner: "staltz", completion: { res in
+            switch res {
+            case .success(let gists):
+                for g in gists {
+                    print(g.files)
+                }
+            case .failure(let err):
+                print(err)
+            }
+        })
+        
     }
 
 
